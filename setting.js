63b2b95c372b65eed87de17f3453d086050b2b02
Debanshu129 <debanshu129@gmail.com>
2017-09-08 00:25:18 +0530

    files added
    Snake using HTML canvas ad Javascript

---
+++ setting.js
@@ -0,0 +1,9 @@
+var mycanvas = document.getElementById('mycanvas');
+var ctx = mycanvas.getContext('2d');
+var snakeSize = 10; 
+var w = 1000;
+var h = 750;
+var score = 0;
+var snake;
+var snakeSize = 10;
+var food;
